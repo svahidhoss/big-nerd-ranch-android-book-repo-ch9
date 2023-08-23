f8e0d80984fb7451825554e58cbbd3a44419d249
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-08-23 16:03:04 -0400

    Ch10: Now hardcodes Ch9: Adds the MainActivity to show CrimeListFragment.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -9,7 +9,7 @@ private const val TAG = "CrimeListFragment"
 
 class CrimeListFragment : Fragment() {
 
-    val crimeListViewModel: CrimeListViewModel by viewModels()
+    private val crimeListViewModel: CrimeListViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")

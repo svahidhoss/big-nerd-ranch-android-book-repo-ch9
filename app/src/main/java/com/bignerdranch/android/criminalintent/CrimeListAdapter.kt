d6aba6048bb14b0df9bd86c1e5e382fd504eb651
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-08-24 13:44:51 -0400

    Ch10: Now each list item responds to clicks.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.criminalintent.databinding.ListItemCrimeBinding
 
@@ -20,9 +21,18 @@ class CrimeListAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<C
     }
 }
 
-class CrimeHolder(val binding: ListItemCrimeBinding) : RecyclerView.ViewHolder(binding.root) {
+class CrimeHolder(private val binding: ListItemCrimeBinding) :
+    RecyclerView.ViewHolder(binding.root) {
     fun bind(crime: Crime) {
         binding.crimeTitle.text = crime.title
         binding.crimeDate.text = crime.date.toString()
+        // add a toast for clicking each row
+        binding.root.setOnClickListener {
+            Toast.makeText(
+                binding.root.context,
+                "${crime.title} clicked!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }

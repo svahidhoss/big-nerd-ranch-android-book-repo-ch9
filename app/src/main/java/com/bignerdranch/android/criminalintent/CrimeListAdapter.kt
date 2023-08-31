ebfc50b3f51d8db097b587ec2a1e1f8391330dee
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-08-31 16:56:50 -0400

    Ch11: Makes the visibility of list item's crimeSolved image view icon dynamic.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListAdapter.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -34,5 +35,7 @@ class CrimeHolder(private val binding: ListItemCrimeBinding) :
                 Toast.LENGTH_SHORT
             ).show()
         }
+        binding.crimeSolved.visibility =
+            if (crime.isSolved) View.VISIBLE else View.GONE
     }
 }

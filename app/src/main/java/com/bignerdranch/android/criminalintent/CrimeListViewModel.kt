4e65614fdab2c7b40da8bfc128cc0026eebcd844
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-01 13:05:40 -0400

    Ch12: Updates CrimeListViewModel.kt and adds the suspended function load crimes that run in view model scope.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -1,13 +1,30 @@
 package com.bignerdranch.android.criminalintent
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.Date
 import java.util.UUID
+
+private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
 
     init {
+        Log.d(TAG, "init method starting")
+        viewModelScope.launch {
+            Log.d(TAG, "coroutine launched!")
+            crimes += loadCrimes()
+            Log.d(TAG, "Loading crimes finished")
+        }
+    }
+
+    private suspend fun loadCrimes(): List<Crime> {
+        val result = mutableListOf<Crime>()
+        delay(5000)
         // populate with dummy data
         for (i in 0 until 100) {
             val crime = Crime(
@@ -16,7 +33,8 @@ class CrimeListViewModel : ViewModel() {
                 date = Date(),
                 isSolved = i % 2 == 0
             )
-            crimes += crime
+            result += crime
         }
+        return result
     }
 }

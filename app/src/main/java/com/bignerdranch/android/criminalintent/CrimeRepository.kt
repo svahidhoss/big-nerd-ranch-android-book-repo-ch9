dd9cd83d681a371abed581698fb56ef26ee8c317
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-01 17:34:16 -0400

    Ch12: Adds the db instance to the CrimeRepository class. Adds repo functions to it.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -1,8 +1,22 @@
 package com.bignerdranch.android.criminalintent
 
 import android.content.Context
+import androidx.room.Room
+import com.bignerdranch.android.criminalintent.database.CrimeDatabase
+import java.util.UUID
+
+private const val DATABASE_NAME = ""
 
 class CrimeRepository private constructor(context: Context) {
+
+    private val database = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    suspend fun getCrimes(): List<Crime> = database.crimeDao().getCrimes()
+    suspend fun getCrime(id: UUID): Crime = database.crimeDao().getCrime(id)
 
     companion object {
         private var INSTANCE: CrimeRepository? = null

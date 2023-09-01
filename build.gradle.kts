d35c00cef7d26c2ab2b15914585f9116d3882ee9
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-01 15:45:49 -0400

    Ch12: Introduces the gradle clean task to the project's root build file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}

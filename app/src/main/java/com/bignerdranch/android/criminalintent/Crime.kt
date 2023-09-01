47bdbbdde667cf4fbaad2c534c6e9acf152a6dff
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-01 17:08:24 -0400

    Ch12: Makes Crime data class and entity. Adds CrimeDatabase class and links it to CrimeTypeConverters class. Hookup CrimeDao to CrimeDatabase class by adding a CrimeDao function.

--- app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -1,13 +1,16 @@
 package com.bignerdranch.android.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.Date
 import java.util.UUID
 
 /**
  * Represents a single office crime.
  */
+@Entity
 data class Crime(
-    val id: UUID,
+    @PrimaryKey val id: UUID,
     val title: String,
     val date: Date,
     val isSolved: Boolean

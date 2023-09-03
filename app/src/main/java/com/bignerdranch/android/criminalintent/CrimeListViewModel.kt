aceeb8019ce89d381345d7256f4ad4dc10b83c90
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-02 21:02:35 -0400

    Ch12: Now caches the crimes list efficently from DB in VM

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListViewModel.kt
@@ -2,6 +2,9 @@ package com.bignerdranch.android.criminalintent
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListViewModel"
@@ -9,10 +12,15 @@ private const val TAG = "CrimeListViewModel"
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
 
-    val crimes = crimeRepository.getCrimes()
+    private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
+    val crimes: StateFlow<List<Crime>>
+        get() = _crimes.asStateFlow()
 
     init {
         viewModelScope.launch {
+            crimeRepository.getCrimes().collect {
+                _crimes.value = it
+            }
         }
     }
 }

8ee648a777e032d078c2a18277d82a734f764034
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-08 12:45:54 -0700

    Ch13: Adds navigation component lib dependencies and updates the lib versions for the build.gradle.kts file. Adds an empty navigation graph file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,23 +43,27 @@ android {
 dependencies {
     val fragmentVersion = "1.6.1"
     val roomVersion = "2.5.2"
+    val androidxLifeCycleVersion = "2.6.2"
 
     // Jetpack
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$androidxLifeCycleVersion")
     implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
     // coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$androidxLifeCycleVersion")
     // Room
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
+    // navigation component lib
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
 
     // test
     testImplementation("junit:junit:4.13.2")

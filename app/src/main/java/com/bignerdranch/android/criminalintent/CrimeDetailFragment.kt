6f27cbeac0a1e98fad9058692f5ac4f6a5c99986
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2023-09-11 13:59:01 -0700

    Ch13: Cleans the boilerplate code from initiation.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDetailFragment.kt
@@ -13,17 +13,10 @@ import com.bignerdranch.android.criminalintent.databinding.FragmentCrimeDetailBi
 import java.util.Date
 import java.util.UUID
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 private const val TAG = "CrimeDetailFragment"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [CrimeDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
 class CrimeDetailFragment : Fragment() {
 
@@ -39,10 +32,6 @@ class CrimeDetailFragment : Fragment() {
 
     private val args: CrimeDetailFragmentArgs by navArgs()
 
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime(
@@ -53,11 +42,6 @@ class CrimeDetailFragment : Fragment() {
         )
 
         Log.d(TAG, "The crime ID is: ${args.crimeId}")
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -90,25 +74,5 @@ class CrimeDetailFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CrimeDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CrimeDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
